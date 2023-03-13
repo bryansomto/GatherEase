901cc14becaf862cd5f54755ce65c9011dbd5c74
bryansomto <bryansomto@gmail.com>
2023-03-13 22:26:11 +0100

    add new styles

--- frontend/src/Components/Auth/AuthForm.js
+++ frontend/src/Components/Auth/AuthForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form } from "../styles";
+import { Main, Form } from "../styles";
 const body = {
   username: "",
   firstname: "",
@@ -16,31 +16,33 @@ export const LoginForm = () => {
     setData({ ...data, [name]: value });
   };
   return (
-    <Form>
-      <header>Login</header>
-      <div className="input">
-        <label htmlFor="name"> Username </label>
-        <input
-          type="text"
-          name="name"
-          placeholder="(e.g). JohnDoe"
-          value={data.username}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="input">
-        <label htmlFor="password"> password </label>
-        <input
-          type="password"
-          name="password"
-          value={data.password}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="submit">
-        <input type="submit" value="send" />
-      </div>
-    </Form>
+    <Main>
+      <Form>
+        <header>Login</header>
+        <div className="input">
+          <label htmlFor="name"> Username </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="(e.g). JohnDoe"
+            value={data.username}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="password"> password </label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <button className="authButton" type="button" value="send">
+          Send
+        </button>
+      </Form>
+    </Main>
   );
 };
 
@@ -51,59 +53,59 @@ export const SignUpForm = () => {
     setData({ ...data, [name]: value });
   };
   return (
-    <Form>
-      <header>Register</header>
-      <div className="input">
-        <label htmlFor="name"> First Name </label>
-        <input
-          type="text"
-          name="firstname"
-          placeholder="(e.g). John"
-          value={data.firstname}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="input">
-        <label htmlFor="name"> Last Name </label>
-        <input
-          type="text"
-          name="lastname"
-          placeholder="(e.g). Doe"
-          value={data.lastname}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="input">
-        <label htmlFor="email"> email </label>
-        <input
-          type="email"
-          name="email"
-          placeholder="abc@gmail.com"
-          value={data.email}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="input">
-        <label htmlFor="password1"> password </label>
-        <input
-          type="password"
-          name="password1"
-          value={data.password1}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="input">
-        <label htmlFor="password2"> confirm password </label>
-        <input
-          type="password"
-          name="password2"
-          value={data.password2}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="submit">
-        <input type="submit" value="send" />
-      </div>
-    </Form>
+    <Main>
+      <Form>
+        <header>Register</header>
+        <div className="input">
+          <label htmlFor="name"> First Name </label>
+          <input
+            type="text"
+            name="firstname"
+            placeholder="(e.g). John"
+            value={data.firstname}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="name"> Last Name </label>
+          <input
+            type="text"
+            name="lastname"
+            placeholder="(e.g). Doe"
+            value={data.lastname}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="email"> email </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="abc@gmail.com"
+            value={data.email}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="password1"> password </label>
+          <input
+            type="password"
+            name="password1"
+            value={data.password1}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="password2"> confirm password </label>
+          <input
+            type="password"
+            name="password2"
+            value={data.password2}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <button className="submit" type="button" value="send" />
+      </Form>
+    </Main>
   );
 };
--- frontend/src/Components/styles.js
+++ frontend/src/Components/styles.js
@@ -1,31 +1,37 @@
-import styled from 'styled-components'
-import tw from 'twin.macro'
-export const Main =styled.section`
-${tw`flex flex-col items-center p-12 md:p-24 py-12 space-y-8`}
-`
+import styled from "styled-components";
+import tw from "twin.macro";
+export const Main = styled.section`
+  ${tw`flex flex-col items-center p-12 md:p-24 py-12 space-y-8`}
+`;
 export const Form = styled.form`
-${tw`w-full max-w-[500px] flex flex-col space-y-5`}
-header{
-    font-family:PoppinsBold;
+  ${tw`w-full max-w-[350px] flex flex-col space-y-5`}
+  header {
+    font-family: PoppinsBold;
     ${tw`capitalize`}
-}
-.submit, .input{
+  }
+  .submit,
+  .input {
     ${tw`w-full flex items-start flex-col space-y-2`}
-    label{
-        font-family:PoppinsSemiBold;
-        ${tw`text-[rgba(0,0,0,.7)] capitalize text-sm`}
-    }textarea, input{
-        font-family:DMsansRegular;
-        ${tw`w-full py-2 px-5 border border-[rgba(0,0,0,.5)] text-[rgba(0,0,0,.7)] rounded-lg`}
-        :placeholder{
-            ${tw`capitalize text-[rgba(0,0,0,.7)]`}
-        }
+    label {
+      font-family: PoppinsSemiBold;
+      ${tw`text-[rgba(0,0,0,.7)] capitalize text-sm`}
     }
-}
-.submit{
+    textarea,
+    input {
+      font-family: DMsansRegular;
+      ${tw`w-full py-2 px-5 border border-[rgba(0,0,0,.5)] text-[rgba(0,0,0,.7)] rounded-lg`}
+      :placeholder {
+        ${tw`capitalize text-[rgba(0,0,0,.7)]`}
+      }
+    }
+  }
+  .submit {
     ${tw`items-end`}
-    input{
-        ${tw`w-auto cursor-pointer py-2.5 px-5 bg-newGreen text-white rounded-lg`}
+    input {
+      ${tw`w-auto cursor-pointer py-2.5 px-5 bg-newGreen text-white rounded-lg`}
     }
-}
-`
+  }
+  .authButton {
+    ${tw`shadow bg-indigo-900 hover:bg-indigo-800 text-white py-3`}
+  }
+`;
