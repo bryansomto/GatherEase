413b8e3d202487f77812a0e1287dad9b5f600fb8
kevohm <kevinkbet@gmail.com>
2023-03-22 18:52:03 +0300

    added protection to some routes

--- frontend/src/Pages/ProtectedRoutes.js
+++ frontend/src/Pages/ProtectedRoutes.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Navigate, Outlet } from 'react-router'
+import { useGlobally } from '../context/AppContext'
 const ProtectedRoutes = () => {
+    const {user} = useGlobally()
+    if(!user){
+        return <Navigate to="/"/>
+    }
   return <Outlet/>
 }
 
